d6a8010503a4961e9267aaf71e4e649740c0e166
mazlumemre <mazlumemregirgin@msn.com>
2025-02-05 21:53:33 +0300

    pages mapped

--- src/main/java/com/sib/sayitbettr/pageController/pageController.java
+++ src/main/java/com/sib/sayitbettr/pageController/pageController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class pageController {
 
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
