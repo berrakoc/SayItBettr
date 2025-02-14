70584422c44a806de2c008511fb5d161f36ed003
mazlumemre <mazlumemregirgin@msn.com>
2025-02-14 22:52:15 +0300

    word service added

--- src/main/java/com/sib/sayitbettr/Service/WordService.java
+++ src/main/java/com/sib/sayitbettr/Service/WordService.java
@@ -1,4 +1,22 @@
 package com.sib.sayitbettr.Service;
 
+import com.sib.sayitbettr.Model.Word;
+import com.sib.sayitbettr.Repository.WordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class WordService {
+    private final WordRepository wordRepository;
+
+    @Autowired
+    public WordService(WordRepository wordRepository) {
+        this.wordRepository = wordRepository;
+    }
+
+    public List<Word> getWordsByLevel(int level) {
+        return wordRepository.findByLevel(level);
+    }
 }
+
