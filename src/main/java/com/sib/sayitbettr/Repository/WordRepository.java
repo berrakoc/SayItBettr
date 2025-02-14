3e53fcd355694fd3391bca263c66a5fcdadbb30e
mazlumemre <mazlumemregirgin@msn.com>
2025-02-14 22:52:39 +0300

    word repository added

--- src/main/java/com/sib/sayitbettr/Repository/WordRepository.java
+++ src/main/java/com/sib/sayitbettr/Repository/WordRepository.java
@@ -1,4 +1,12 @@
 package com.sib.sayitbettr.Repository;
 
-public interface WordRepository {
+import com.sib.sayitbettr.Model.Word;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface WordRepository extends JpaRepository<Word, Long> {
+    List<Word> findByLevel(int level);
 }
+
